a3fb58f9fc6ddc2ef955632cb23f2869fbc35946
MelaniS90 <magdalena.lutynska98@gmail.com>
2023-06-21 01:41:17 +0200

    post rest client

--- lib/services/postRestClient.dart
+++ lib/services/postRestClient.dart
@@ -1,19 +1,55 @@
+import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../data/post.dart';
 import 'package:http/http.dart' as http;
 
 class PostRestService {
-  Post post;
-  var url = Uri.https('/post');
+  Post? post;
+  var lat;
+  var lang;
+
+  var accessToken;
   var client = http.Client();
+  
+  var hostName = "https://postspot-api-gateway-eu-dev-v1-0-8-a5mqnrt6.nw.gateway.dev";
+  var endpoint = "/v1/posts";
+  Map<String,String> getQueryParameters = {};
 
-  PostRestService(this.post);
+  PostRestService(post, lat, lang){
+    this.post = post;
+    this.lat = lat;
+    this.lang = lang;
+    this.accessToken = FirebaseAuth.instance.currentUser!.refreshToken;
+    getQueryParameters['lat'] = lat;
+    getQueryParameters['lang'] = lang;
+  }
 
-  Future<void> sendRequest() async {
-    final jsonData = post.toJson();
+  Future<void> createPost() async {
+    final jsonData = post!.toJson();
     try {
-      var response = await client.post(url, body: jsonData);
+      var response = await client.post(
+        Uri.https(hostName,endpoint),
+        headers:{
+          'token' : accessToken
+        }, 
+         body: jsonData);
     } finally {
       client.close();
     }
   }
+
+  Future<void> getPosts() async {
+    try {
+      var response = await client.get(
+        Uri.https(hostName,endpoint,getQueryParameters),
+        headers:{
+          'token' : accessToken
+        });
+    } finally {
+      client.close();
+    }
+  }
+
 }
+
+
